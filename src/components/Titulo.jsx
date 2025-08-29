a43f3aac4ce26b98efe9059f3a549970a81a1968
miguel <mespi763@gmail.com>
2025-08-29 18:04:48 -0300

    parte 1

---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Titulo() {
+  return (
+      <div>
+        <h1>Mi Perfil Profesional</h1>
+      <p>Bienvenido a mi portafolio de desarrollador web</p>
+      </div>
+      
+      )
+}
